3674cb4d0f1152dcf92511f53ce9e5296a4b8bd7
abdulsamad-11 <72301059+abdulsamad-11@users.noreply.github.com>
2020-10-31 15:28:22 +0500

    start assignment 5

---
+++ assignment 5_Roll No PIAIC166153/task2 i .js
@@ -0,0 +1,13 @@
+
+//argument as value
+
+
+function area (width,height){
+    area=width*height;
+    console.log("arguments as value " +area);
+}
+area(20,10);
+
+
+
+
---
+++ assignment 5_Roll No PIAIC166153/task2 ii .js
@@ -0,0 +1,10 @@
+//argument as variable
+
+
+function area (){
+    var width = 20;
+    var height = 10;
+    area=width*height;
+    console.log("arguments as variable " +area);
+}
+area();
