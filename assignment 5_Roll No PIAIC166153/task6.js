6e60d8bbbe209d3c26ffc470e67348cd4ea4723e
abdulsamad-11 <72301059+abdulsamad-11@users.noreply.github.com>
2020-11-02 03:55:44 +0500

    ass 5 task 5

---
+++ assignment 5_Roll No PIAIC166153/task6.js
@@ -0,0 +1,34 @@
+
+function arr(){
+    var userInput = prompt("enter number");
+     var arString  = userInput.toString();
+     var ardivde = userInput.split("");
+     var arrcli1 = ardivde.slice("");
+     var armiror = ardivde.reverse("");
+     var mirrcli = armiror.slice("");
+     var fineArry= (arrcli1 +","+ mirrcli);
+     console.log(fineArry);
+}
+arr();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
